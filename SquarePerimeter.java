c01a850c1a3ef09e6d274911c2430780c8389c13
vaishnavichandode <119327988+vaishnavichandode@users.noreply.github.com>
2025-01-14 09:52:45 +0530

    Add files via upload

---
+++ SquarePerimeter.java
@@ -0,0 +1,30 @@
+// write a program to find the perimeter of the square 
+
+// logic :
+
+import java.util.Scanner;
+
+public class SquarePerimeter {
+    public static void main(String[] args) {
+        //  Scanner object is created 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the side length of the square: ");
+        double side = sc.nextDouble();
+
+        // Calculate the perimeter of the square
+        double perimeter = 4 * side;
+
+        // Display the perimeter
+        System.out.println("The perimeter of the square is: " + perimeter);
+
+      
+    }
+}
+
+
+
+// Output 
+
+Enter the side length of the square: 5
+The perimeter of the square is: 20.0
